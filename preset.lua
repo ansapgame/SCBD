0aeb49e1327bb648f53ae7bb9d071f487a16baea
ansapgame <ansapgame@gmail.com>
2026-08-16 18:56:45 +0700

    Update preset.lua

--- preset.lua
+++ preset.lua
@@ -1,18 +1,3 @@
 	return {
-	["Mayor Pass"] = {
-		["Season 1 - Americana"] = {
-			["Season 1 - Americana - Part 1"] = {
-			{ id = -696532419, name = "SimCity Bank" },
-			{ id = -970872132, name = "Backyard Basketball" },
-			{ id = -177840672, name = "Crispy Clean Laundry Wash" },
-			{ id = -2017049381, name = "SimCity Telephone Company" },
-			{ id = 1936070297, name = "Rustling Pages Bookstore" },
-			{ id = -2105219743, name = "SimCity Newspaper Office" },
-			{ id = -2141420504, name = "Hair and Clippers Salon" },
-			{ id = 1085959474, name = "Post Office" },
-			{ id = -228481548, name = "Sunset Diner" },
-			{ id = 1500570218, name = "Roller Skating Lounge" },
-			{ id = -557351818, name = "Firewheel Lounge" },
-		},
-},
+
 }
